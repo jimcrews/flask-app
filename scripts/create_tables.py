4f189a2b85016b8aa5a250ec62c628173b30ad01
Jim Crews <crews.jim@gmail.com>
2020-11-22 17:07:05 +1100

    items in db

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -1,11 +1,15 @@
 import sqlite3
 
 connection = sqlite3.connect('../data.db')
-curser = connection.cursor()
+cur = connection.cursor()
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-curser.execute(create_table)
+create_users = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cur.execute(create_users)
 
+create_items = "CREATE TABLE IF NOT EXISTS items (name test, price real)"
+cur.execute(create_items)
+
+cur.execute("INSERT INTO items VALUES ('test', 10.99)")
 
 connection.commit()
 connection.close()
