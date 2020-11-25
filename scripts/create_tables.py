4f39d429a4c2f0d7b746e801b6fb729d3c282118
Jim Crews <crews.jim@gmail.com>
2020-11-26 10:07:13 +1100

    update db

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -6,7 +6,7 @@ cur = connection.cursor()
 create_users = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cur.execute(create_users)
 
-create_items = "CREATE TABLE IF NOT EXISTS items (name test, price real)"
+create_items = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name test, price real)"
 cur.execute(create_items)
 
 
