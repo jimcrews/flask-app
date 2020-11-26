660cd32c4200e83fd8402fc033b85ef198bc437e
Jim Crews <crews.jim@gmail.com>
2020-11-26 12:08:24 +1100

    implement user model

--- models/user.py
+++ models/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import db
 
 
@@ -10,8 +9,7 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, id, username, password):
-        self.id = id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
@@ -20,32 +18,12 @@ class UserModel(db.Model):
 
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT * FROM users where username=?'
-        result = cursor.execute(query, (username,))  # param must be a tuple
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(id=_id).first()
 
-        query = 'SELECT * FROM users where id=?'
-        result = cursor.execute(query, (_id,))  # param must be a tuple
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
